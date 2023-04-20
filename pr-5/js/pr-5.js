f05d15b2df067d0b0c510420fe99caf4d8b4a2d9
o0David0o <git.hub.o0.d.0o@gmail.com>
2023-04-20 14:04:47 +0300

    upd

--- pr-5/js/pr-5.js
+++ pr-5/js/pr-5.js
@@ -1,25 +1,25 @@
 class Factorial {
     constructor(n) {
-        let res = NaN
-        try {
-    
-            if (Number.isInteger(n) && n > 0) {
-                if (n === 0 || n === 1) {
-                    res = 1
-                }
-                else {
-                    res = n * Factorial(n - 1)
-                }
-            } else {
-                throw new Error('Invalid input data')
-            }
-        }
-        catch (error) {
-            console.error(error)
-        }
-        return res
+        return this.calculate(n)
     }
+    
+    calculate(n) {
+        res = NaN;
+        if (Number.isInteger(n) && n > 0) {
+            if (n === 0 || n === 1) {
+                res = 1;
+            } else {
+                res = n * Factorial(n - 1)
+            }
+        } else {
+            throw new Error('Invalid factorial')
+        }
+        
+        return res; 
+    } 
 }
+
+const fac = Factorial(3)
 
 class QuadEquation { }
 
@@ -27,15 +27,15 @@ class QuadEquation { }
 
 // function factrorial($n)
 // {
-//     $res = false;
+//     res = false;
 //     if (is_int($n) and $n > 0) {
 
 //         if ($n == 0 or $n == 1) {
-//             $res = 1;
+//             res = 1;
 //         } else {
-//             $res = $n * factrorial($n - 1);
+//             res = $n * factrorial($n - 1);
 //         }
 //     }
 
-//     return $res;
+//     return res;
 // }
