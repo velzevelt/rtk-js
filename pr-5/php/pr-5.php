3d4f092ba35e1c02bac4533f92532af92ef2e6bb
o0David0o <git.hub.o0.d.0o@gmail.com>
2023-04-26 14:40:27 +0300

    upd

--- pr-5/php/pr-5.php
+++ pr-5/php/pr-5.php
@@ -2,5 +2,53 @@
 
 class QuadEquation
 {
-    
+    public $a, $b, $c;
+
+    public function __construct($a=1, $b=1, $c=1) {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+    }
+
+    public function solveEquation() {
+        $res = false;
+        $args = [$this->a, $this->b, $this->c];
+
+        $full = empty(array_filter($args, fn($x) => $x == 0 ));
+
+        if ($full) {
+            $d = $this->b ** 2 - 4 * $this->a * $this->c;
+            if ($d <= 0) {
+                $res = ($d === 0 && $this->a !== 0) ? [-$this->b / 2 * $this->a] : false;
+            } 
+            else {
+                $d_root = sqrt($d);
+                $t = 2 * $this->a;
+                $res = [(-$this->b + $d_root) / $t, (-$this->b - $d_root) / $t];
+            }
+        } else if (empty( array_filter( $args, fn($x) => is_int($x) ) )) {
+            $caseC = $this->c == 0 && $this->a != 0 && $this->b != 0;
+            $caseBC = $this->b == 0 && $this->c == 0 && $this->a != 0;
+
+            $t = -$this->c / $this->a;
+            $caseB = $this->b == 0 && $this->a != 0 && $this->c != 0 && $t > 0;
+
+            if ($caseB) {
+                $res = [-sqrt($t), sqrt($t)];
+            } else if ($caseC) {
+                $res = [0, -$this->b / $this->a];
+            } else if ($caseBC) {
+                $res = [0];
+            }
+        }
+
+        return $res;
+    }
 }
+
+
+$eq = new QuadEquation(1, -8, 12);
+var_dump($eq->solveEquation());
+
+$eq = new QuadEquation(1, -8, 12);
+var_dump($eq->solveEquation());
--- pr-9/QuadEquationTest.php
+++ pr-9/QuadEquationTest.php
@@ -1,4 +1,14 @@
 <?php
 require_once 'vendor/autoload.php';
 
-use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestCase as TestCase;
+
+final class QuadEquationTest extends TestCase
+{
+    public function testValidCalculation()
+    {
+        $eq = new QuadEquation(1, -2, 3).solveEquation();
+        $solve = [-1, 3];
+        $this->assertSame($eq, $solve);
+    }
+}
