8d3b5ec4f05c9d58ffaaf5331527ec412d37d5f9
Class_Student <Class_Student@rtk.local>
2023-09-26 17:26:37 +0300

    3 5 upd

--- pr-3/pr-3.js
+++ pr-3/pr-3.js
@@ -19,289 +19,130 @@
 */
 
 class TurtleRunner {
-    runH = 4
-    speedKpH = 2
-    initSpeedKpH = this.speedKpH
+  runH = 4;
+  speedKpH = 2;
+  initSpeedKpH = this.speedKpH;
 
-    runDistanceK = 40
-    hourSimulationMilSeconds = 320
+  runDistanceK = 40;
+  hourSimulationMilSeconds = 320;
 
-    sleepH = 0
+  sleepH = 0;
 
-    totalRunH = 0
-    totalSleepH = 0
-    finished = false
+  totalRunH = 0;
+  totalSleepH = 0;
+  finished = false;
 
-    constructor(name) {
-        this.name = name
+  constructor(name) {
+    this.name = name;
+  }
+
+  startRace() {
+    setTimeout(() => this.#run(), this.hourSimulationMilSeconds);
+  }
+
+  #run() {
+    this.runDistanceK -= this.speedKpH;
+    this.runH--;
+    this.totalRunH++;
+
+    //#region Логирование
+    let runMessage = "";
+    if (this?.name) {
+      runMessage += `Черепаха "${this.name}": `;
+    }
+    runMessage += `Осталось бежать: ${this.runDistanceK} км. Скорость: ${this.speedKpH}. Могу бежать еще ${this.runH} ч`;
+    console.log(runMessage);
+    //#endregion
+
+    if (this.speedKpH > this.initSpeedKpH) {
+      this.speedKpH--;
     }
 
+    if (this.runDistanceK <= 0) {
+      this.finished = true;
+      this.#showRaceTotal();
+    } else {
+      if (this.runH > 0) {
+        setTimeout(() => this.#run(), this.hourSimulationMilSeconds);
+      } else {
+        // this.sleepH = 5
+        this.sleepH = getRandomInt(3, 5);
+        setTimeout(() => this.#sleep(), this.hourSimulationMilSeconds);
+      }
+    }
+  }
 
-    startRace() {
-        // Из-за потери контекста, неободимо передавать функцию как () => this.funcName()
-        setTimeout(() => this.#run(), this.hourSimulationMilSeconds)
+  #sleep() {
+    if (this.runH < 4) {
+      this.runH++;
+    } else {
+      this.speedKpH++;
     }
 
-    #run() {
-        this.runDistanceK -= this.speedKpH
-        this.runH--
-        this.totalRunH++
+    this.sleepH--;
+    this.totalSleepH++;
 
-
-
-        //#region Логирование
-        let runMessage = ''
-        if (this?.name) {
-            runMessage += `Черепаха "${this.name}": `
-        }
-        runMessage += `Осталось бежать: ${this.runDistanceK} км. Скорость: ${this.speedKpH}. Могу бежать еще ${this.runH} ч`
-        console.log(runMessage)
-        //#endregion
-
-
-        if (this.speedKpH > this.initSpeedKpH) {
-            this.speedKpH--
-        }
-
-
-        if (this.runDistanceK <= 0) {
-            this.finished = true
-            this.#showRaceTotal()
-        }
-        else {
-            if (this.runH > 0) {
-                setTimeout(() => this.#run(), this.hourSimulationMilSeconds)
-            }
-            else {
-                // this.sleepH = 5
-                this.sleepH = getRandomInt(3, 5)
-                setTimeout(() => this.#sleep(), this.hourSimulationMilSeconds)
-            }
-        }
-
+    //#region Логирование
+    let sleepMessage = "";
+    if (this?.name) {
+      sleepMessage += `Черепаха "${this.name}": `;
     }
+    sleepMessage += `Осталось спать: ${this.sleepH}`;
+    console.log(sleepMessage);
+    //#endregion
 
-    #sleep() {
-        if (this.runH < 4) {
-            this.runH++
-        }
-        else {
-            this.speedKpH++
-        }
-
-        this.sleepH--
-        this.totalSleepH++
-
-        //#region Логирование
-        let sleepMessage = ''
-        if (this?.name) {
-            sleepMessage += `Черепаха "${this.name}": `
-        }
-        sleepMessage += `Осталось спать: ${this.sleepH}`
-        console.log(sleepMessage)
-        //#endregion
-
-        if (this.sleepH > 0) {
-            setTimeout(() => this.#sleep(), this.hourSimulationMilSeconds)
-        }
-        else {
-            setTimeout(() => this.#run(), this.hourSimulationMilSeconds)
-        }
-
+    if (this.sleepH > 0) {
+      setTimeout(() => this.#sleep(), this.hourSimulationMilSeconds);
+    } else {
+      setTimeout(() => this.#run(), this.hourSimulationMilSeconds);
     }
+  }
 
-
-    #showRaceTotal() {
-        let totalMessage = ''
-        if (this?.name) {
-            totalMessage += `Черепаха "${this.name}": Финиш! `
-        }
-        totalMessage += `Часов пробега: ${this.totalRunH}. Часов сна: ${this.totalSleepH}`
-        console.log(totalMessage)
-        alert(totalMessage)
+  #showRaceTotal() {
+    let totalMessage = "";
+    if (this?.name) {
+      totalMessage += `Черепаха "${this.name}": Финиш! `;
     }
-
+    totalMessage += `Часов пробега: ${this.totalRunH}. Часов сна: ${this.totalSleepH}`;
+    console.log(totalMessage);
+    alert(totalMessage);
+  }
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 function getRandomElement(from) {
-    return from[Math.floor(Math.random() * from.length)]
+  return from[Math.floor(Math.random() * from.length)];
 }
 
 // Задание 1
-// const turtle_1 = new TurtleRunner('Снежанна')
-// const turtle_2 = new TurtleRunner('Анжелина')
-// turtle_1.startRace()
-// turtle_2.startRace()
+const turtle_1 = new TurtleRunner("Снежанна");
+const turtle_2 = new TurtleRunner("Анжелина");
+turtle_1.startRace();
+turtle_2.startRace();
 
-
-
-
-
-
-class Train {
-    hourSimulationMilSeconds = 1200
-    speedKpH = 0
-    travelStatus = 'По расписанию'
-    TrainRoute
-    currentTrainStop
-
-    totalTimeH = 0
-
-    constructor(TrainRoute) {
-        this.TrainRoute = TrainRoute
-        if (TrainRoute) {
-            this.currentTrainStop = { ...TrainRoute.stops[0] } //* Нельзя менять исходные данные остановки, можно использовать только копии
-            this.speedKpH = Math.round(this.currentTrainStop.nextStopDistanceK / this.currentTrainStop.plannedTravelTimeH)
-        }
-        else {
-            console.error('Train init error. Check passed TrainRoute')
-        }
-    }
-
-    startMoving() {
-        setTimeout(() => this.#move(), this.hourSimulationMilSeconds)
-    }
-
-    #move() {
-        if (this.currentTrainStop.stopTimeH > 0) {
-            const delay = this.currentTrainStop.stopTimeH > this.currentTrainStop.plannedStopTimeH
-            if (delay) {
-                console.log(`Остановка ${this.currentTrainStop.stopName} (Возникла задержка): идет смена локомотива, осталось ждать ${this.currentTrainStop.stopTimeH} ч`)
-            }
-            else {
-                console.log(`Остановка ${this.currentTrainStop.stopName}: идет смена локомотива, осталось ждать ${this.currentTrainStop.stopTimeH} ч`)
-            }
-
-            this.currentTrainStop.stopTimeH--
-            this.totalTimeH++
-
-            setTimeout(() => this.#move(), this.hourSimulationMilSeconds)
-        }
-        else if (this.currentTrainStop.nextStopDistanceK > 0) {
-            if (this.currentTrainStop?.nextStop?.stopName) {
-                console.log(`Едем ${this.travelStatus} (${this.speedKpH} км/ч), следующая остановка ${this.currentTrainStop.nextStop.stopName}, осталось ехать: ${this.currentTrainStop.nextStopDistanceK} км`)
-            }
-            else {
-                console.log(`Едем ${this.travelStatus} (${this.speedKpH} км/ч), осталось ехать: ${this.currentTrainStop.nextStopDistanceK} км`)
-            }
-
-            this.totalTimeH++
-            this.currentTrainStop.nextStopDistanceK -= this.speedKpH
-
-            setTimeout(() => this.#move(), this.hourSimulationMilSeconds)
-        }
-        else {
-            if (this.currentTrainStop?.nextStop) {
-                this.currentTrainStop = { ...this.currentTrainStop.nextStop }
-                if (this.currentTrainStop.stopTimeH > this.currentTrainStop.plannedStopTimeH) {
-                    this.travelStatus = "Скоростной режим"
-                    this.speedKpH = Math.ceil(this.currentTrainStop.nextStopDistanceK / this.currentTrainStop.stopTimeH)
-                }
-                else {
-                    this.travelStatus = "По расписанию"
-                    this.speedKpH = Math.round(this.currentTrainStop.nextStopDistanceK / this.currentTrainStop.plannedTravelTimeH)
-                }
-
-                this.#move()
-            }
-            else {
-                console.log(`Приехали`)
-                this.#showTotal()
-            }
-        }
-    }
-
-    #showTotal() {
-        let abberation = this.TrainRoute.totalTimeH - this.totalTimeH
-        if (abberation > 0) {
-            abberation = `Поезд прибыл раньше на ${abberation} ч`
-        } else if (abberation == 0) {
-            abberation = `Поезд прибыл согласно расписанию`
-        } else {
-            abberation = `Поезд опоздал на ${Math.abs(abberation)} ч`
-        }
-
-        const message = `Маршрут занял ${this.totalTimeH} ч, Преодолено: ${this.TrainRoute.totalTravelDistanceK} км, Ожидаемое время пути: ${this.TrainRoute.totalTimeH} ч, ${abberation}`
-        console.log(message)
-        alert(message)
-    }
-}
-
-
-// Маршрут
-class TrainRoute {
-    departureCity
-    destinationCity
-    stops
-    totalTravelTimeH = 0 // Итоговое время движения между остановками (по расписанию)
-    totalTimeH = 0
-    totalTravelDistanceK = 0 // Итоговое расстояние движения
-    averageSpeedKpH = 0
-
-    constructor(departureCity, destinationCity) {
-        this.departureCity = departureCity
-        this.destinationCity = destinationCity
-
-        const initTrainStop = new TrainStop()
-        this.stops = [initTrainStop]
-        this.totalTravelTimeH += initTrainStop.plannedStopTimeH
-        this.totalTravelDistanceK += initTrainStop.nextStopDistanceK
-
-        for (let i = 1; i < getRandomInt(1, 3); i++) {
-            const nextTrainStop = new TrainStop()
-
-            this.stops[i - 1].nextStop = nextTrainStop
-            this.stops.push(nextTrainStop)
-
-            this.totalTravelTimeH += nextTrainStop.plannedStopTimeH
-            this.totalTravelDistanceK += nextTrainStop.nextStopDistanceK
-        }
-
-        this.averageSpeedKpH = Math.round(this.totalTravelDistanceK / this.totalTravelTimeH)
-
-        this.stops.forEach(element => {
-            element.plannedTravelTimeH = Math.round(element.nextStopDistanceK / this.averageSpeedKpH) + element.plannedStopTimeH
-
-            let offset = 0
-            if (Math.random() * 100 > 75) // 25% -> Вероятность задержки на 1 час
-            {
-                offset++
-            }
-
-            element.stopTimeH = element.plannedStopTimeH + offset
-            this.totalTimeH += element.plannedTravelTimeH + element.stopTimeH
-        });
-    }
-
-}
-
-
-// Остановка/Станция
-class TrainStop {
-    stopName
-    plannedStopTimeH // Планируемая длительность остановки
-    stopTimeH // Длительность остановки
-    nextStop //* Следующая остановка
-    nextStopDistanceK // Расстояние до следующей остановки
-    plannedTravelTimeH // Планируемая длительность движения до следующей остановки
-
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')
+class TrainTimer {
+    t
+    totalTimeMin = 0;
 
     constructor() {
-        this.stopName = getRandomElement(this.alphabet) + getRandomInt(1, 100)
-        this.nextStopDistanceK = getRandomInt(80, 500)
-        this.plannedStopTimeH = getRandomInt(1, 3)
+        
     }
+
 }
 
+class Train {
 
+
+}
 
 // Задание 2
-const train = new Train(new TrainRoute('N', 'M'))
-train.startMoving()
+// const train = new Train(new TrainRoute('N', 'M'))
+// train.startMoving()
+// 1200:1ч .. 1200:60 
+
+// 1:20
--- pr-5/js/pr-5.js
+++ pr-5/js/pr-5.js
@@ -1,122 +1,124 @@
 class InvalidFactorialError extends Error {}
 class InvalidArgumentTypeError extends Error {}
-
+class InvalidEquation extends Error {}
 
 class Factorial {
-    constructor(n) {
-        this.n = n
+  constructor(n) {
+    this.n = n;
+  }
+
+  calculate() {
+    let res = NaN;
+
+    if (Number.isInteger(this.n) && this.n >= 0) {
+      if (this.n === 0 || this.n === 1) {
+        res = 1;
+      } else {
+        res = this.n * new Factorial(this.n - 1).calculate();
+      }
+    } else {
+      throw new InvalidFactorialError(
+        `Неверное число ${this.n}. Факториал не существует`
+      );
     }
 
-    calculate() {
-        let res = NaN;
+    return res;
+  }
 
-        if (Number.isInteger(this.n) && this.n >= 0) {
-            if (this.n === 0 || this.n === 1) {
-                res = 1;
-            } else {
-                res = this.n * new Factorial(this.n - 1).calculate()
-            }
-        } else {
-            throw new InvalidFactorialError(`Неверное число ${this.n}. Факториал не существует`)
-        }
-
-        return res;
+  calculatePrompt() {
+    this.n = Number.parseInt(prompt("Посчитать факториал для n"));
+    let out;
+    try {
+      out = this.calculate();
+    } catch (error) {
+      out = error.message;
     }
 
-    calculatePrompt() {
-        this.n = Number.parseInt(prompt('Посчитать факториал для n'))
-        let out = NaN
-        try {
-            out = this.calculate()
-        } catch (error) {
-            out = error.message
-        }
-       
-
-        alert(out)
-    }
+    alert(out);
+  }
 }
 
-
 class QuadEquation {
-    constructor(a = 0, b = 0, c = 0) {
-        this.a = a
-        this.b = b
-        this.c = c
+  constructor(a = 0, b = 0, c = 0) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  solveEquationPrompt() {
+    this.a = prompt("Введите a: ");
+    this.b = prompt("Введите b: ");
+    this.c = prompt("Введите c: ");
+
+    let out = NaN;
+    try {
+      this.a = parseInt(this.a);
+      this.b = parseInt(this.b);
+      this.c = parseInt(this.c);
+
+      out = this.solveEquation();
+    } catch (error) {
+      out = error.message;
     }
 
-    solveEquationPrompt() {
-        this.a = prompt("Введите a: ");
-        this.b = prompt("Введите b: ");
-        this.c = prompt("Введите c: ");
+    alert(out);
+  }
 
-        let out = NaN;
-        try {
-            this.a = parseInt(this.a);
-            this.b = parseInt(this.b);
-            this.c = parseInt(this.c);
+  solveEquation() {
+    let res = NaN;
 
-            out = this.solveEquation();
-        } catch (error) {
-            out = error.message;
+    // Проверяем, полное ли это квадратное уравнение
+    const args = [this.a, this.b, this.c];
+    const full = args.every((val) => Number.isFinite(val) && val !== 0);
+    if (full) {
+      const d = this.b ** 2 - 4 * this.a * this.c;
+
+      if (d <= 0) {
+        res = d === 0 && this.a !== 0 ? [(-this.b / 2) * this.a] : NaN;
+      } else {
+        const d_root = Math.sqrt(d);
+        const t = 2 * this.a;
+        res = [(-this.b + d_root) / t, (-this.b - d_root) / t];
+      }
+    } else if (args.every((val) => Number.isFinite(val))) {
+      // Неквадратное уравнение
+      if (args.every((val) => val === 0))
+        throw new InvalidEquation("Неверное уравнение");
+
+      const caseA = this.a === 0 && this.b !== 0 && this.c !== 0;
+
+      // см. guide.png
+      const caseC = this.c === 0 && this.a !== 0 && this.b !== 0;
+      const caseBC = this.b === 0 && this.c === 0 && this.a !== 0;
+
+      const t = -this.c / this.a;
+      console.log(t);
+
+      const caseB = this.b === 0 && this.a !== 0 && this.c !== 0 && t > 0;
+
+      if (caseB) {
+        res = [-Math.sqrt(t), Math.sqrt(t)];
+      } else if (caseC) {
+        res = [0, -this.b / this.a];
+      } else if (caseBC) {
+        res = [0];
+      } else if (caseA) {
+        // res = [-this.c/this.b];
+        throw new InvalidEquation("Неверное уравнение");
+      }
+    } else {
+      const imposters = [];
+      args.forEach(function (arg) {
+        if (!Number.isFinite(arg)) {
+          imposters.push(`"${typeof arg}": ${arg}`);
         }
+      });
 
-        alert(out)
+      throw new InvalidArgumentTypeError(
+        `Аргумент этого типа не поддерживается ${imposters}`
+      );
     }
 
-    solveEquation() {
-        let res = NaN
-
-        // Проверяем, полное ли это квадратное уравнение
-        const args = [this.a, this.b, this.c]
-        const full = args.every((val) => Number.isFinite(val) && val !== 0)
-        if (full) {
-
-            const d = this.b ** 2 - 4 * this.a * this.c
-
-            if (d <= 0) {
-                res = (d === 0 && this.a !== 0) ? [-this.b / 2 * this.a] : NaN;
-            } else {
-                const d_root = Math.sqrt(d);
-                const t = 2 * this.a;
-                res = [(-this.b + d_root) / t, (-this.b - d_root) / t];
-            }
-
-        } else if (args.every((val) => Number.isFinite(val))) {
-
-            // Неквадратное уравнение
-            const caseA = this.a === 0 && this.b !== 0 && this.c !== 0
-            
-            // см. guide.png
-            const caseC = this.c === 0 && this.a !== 0 && this.b !== 0
-            const caseBC = this.b === 0 && this.c === 0 && this.a !== 0
-
-            const t = -this.c / this.a
-            const caseB = this.b === 0 && this.a !== 0 && this.c !== 0 && t > 0
-
-            if (caseB) {
-                res = [-Math.sqrt(t), Math.sqrt(t)]
-            } else if (caseC) {
-                res = [0, -this.b / this.a]
-            } else if (caseBC) {
-                res = [0]
-            } else if (caseA) {
-                res = [-this.c/this.b];
-            }
-
-        } else {
-            const imposters = [] 
-            args.forEach( function(arg) {
-                if (!Number.isFinite(arg)) {
-                    imposters.push( `"${typeof arg}": ${arg}` )
-                }
-            });
-            
-            throw new InvalidArgumentTypeError(
-                `Аргумент этого типа не поддерживается ${imposters}`
-            );
-        }
-
-        return res
-    }
+    return res;
+  }
 }
