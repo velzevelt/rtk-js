db35d3df0d7cd49df9c487ff1b4b2466c5c8fd6d
velzevelt <novaraptor13@gmail.com>
2023-10-02 22:05:23 +0300

    upd

--- pr-7/pr-7.js
+++ pr-7/pr-7.js
@@ -4,12 +4,24 @@
  * На выходе приложение выдает число вхождений второй строки в первую.
  */
 
+
 function strIntersect(string, subString) {
 
-    // Преобразование в строку
+    // Проверка аргументов
+    [string, subString].forEach( (arg) => {
+        switch (typeof arg) {
+            case "undefined":
+            case "null":
+            case "object": throw new TypeError(`${arg} is ${typeof arg} and cannot be properly casted to string`);
+            case "string": break;
+            default: console.warn(`${arg} is not a string, but it was auto casted`);
+        }
+    } );
+
+    // Преобазование аргументов в строку
     string += "";
     subString += "";
-    
+
     if (subString.length <= 0) return 0;
 
     let n = 0;
@@ -26,17 +38,39 @@ function strIntersect(string, subString) {
     return n;
 }
 
-console.assert(strIntersect("Ш", "Ш") === 1)
-console.assert(strIntersect("", "") === 0)
-console.assert(strIntersect("Жили-были три китайца: Як, Як-цедрак, Як-цедрак-цедрак-цедрони", "Як") === 3)
 
-
-try{
-    const str_1 = prompt("Первая строка", "")
-    const str_2 = prompt("Строка для поиска", "")
-    alert(strIntersect(str_1, str_2))
-} catch(error) {
-    alert(error)
+function assertException(func, exception) {
+    try {
+        func();
+        console.error(`Expected exception "${exception}" was not raised`)
+    } catch (error) {
+        if (!(error instanceof exception))
+            console.error(`Expected exception "${exception}" was not raised`)
+    }
 }
+
+
+console.assert(strIntersect("Ш", "Ш") === 1);
+console.assert(strIntersect("", "") === 0);
+console.assert(strIntersect("Жили-были три китайца: Як, Як-цедрак, Як-цедрак-цедрак-цедрони", "Як") === 3);
+console.assert(strIntersect("", "123") === 0);
+console.assert(strIntersect("123", "") === 0);
+console.assert(strIntersect(111, 1) === 3);
+console.assert(strIntersect(111, '1') === 3);
+console.assert(strIntersect('111', 1) === 3);
+
+
+assertException( () => strIntersect(undefined, undefined), TypeError );
+assertException( () => strIntersect(null, null), TypeError );
+assertException( () => strIntersect(1, null), TypeError );
+assertException( () => strIntersect(null, 1), TypeError );
+assertException( () => strIntersect([], []), TypeError );
+assertException( () => strIntersect(['a', 'b', 'c'], ['a', 'b', 'c']), TypeError );
+
+
+// const str_1 = prompt("Первая строка", "")
+// const str_2 = prompt("Строка для поиска", "")
+// alert(strIntersect(str_1, str_2))
+
 
 
